8512c2dd215050ac13a64670c58343687d6d713e
SeshTheFellOmen <131706549+SeshTheFellOmen@users.noreply.github.com>
2024-09-16 17:26:31 -0400

    Initial commit of the Flutter calculator app

---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+// lib/main.dart
+import 'package:flutter/material.dart';
+import 'package:expressions/expressions.dart';
+
+void main() {
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _expression = '';
+  String _result = '';
+
+  void _onButtonPressed(String value) {
+    setState(() {
+      if (value == 'C') {
+        _expression = '';
+        _result = '';
+      } else if (value == '=') {
+        try {
+          final expression = Expression.parse(_expression);
+          final evaluator = const ExpressionEvaluator();
+          final result = evaluator.eval(expression, {});
+          _result = ' = $result';
+        } catch (e) {
+          _result = ' = Error';
+        }
+      } else {
+        _expression += value;
+      }
+    });
+  }
+
+  Widget _buildButton(String value) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () => _onButtonPressed(value),
+        child: Text(
+          value,
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GitHub Copilot Calculator'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16),
+              alignment: Alignment.bottomRight,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    _expression,
+                    style: TextStyle(fontSize: 32),
+                  ),
+                  Text(
+                    _result,
+                    style: TextStyle(fontSize: 32, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Row(
+                children: [
+                  _buildButton('7'),
+                  _buildButton('8'),
+                  _buildButton('9'),
+                  _buildButton('/'),
+                ],
+              ),
+              Row(
+                children: [
+                  _buildButton('4'),
+                  _buildButton('5'),
+                  _buildButton('6'),
+                  _buildButton('*'),
+                ],
+              ),
+              Row(
+                children: [
+                  _buildButton('1'),
+                  _buildButton('2'),
+                  _buildButton('3'),
+                  _buildButton('-'),
+                ],
+              ),
+              Row(
+                children: [
+                  _buildButton('0'),
+                  _buildButton('C'),
+                  _buildButton('='),
+                  _buildButton('+'),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
